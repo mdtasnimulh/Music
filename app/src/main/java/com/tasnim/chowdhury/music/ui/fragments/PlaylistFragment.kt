dd1aa1c79d46c18086c07332026412fc69ca9da0
tasnimul.hasan <mdtasnimulh@gmail.com>
2023-12-19 12:54:42 +0600

    create playlist dialog

--- app/src/main/java/com/tasnim/chowdhury/music/ui/fragments/PlaylistFragment.kt
+++ app/src/main/java/com/tasnim/chowdhury/music/ui/fragments/PlaylistFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 import com.tasnim.chowdhury.music.R
 import com.tasnim.chowdhury.music.adapters.PlaylistAdapter
 import com.tasnim.chowdhury.music.databinding.FragmentPlaylistBinding
@@ -27,9 +33,42 @@ class PlaylistFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupAdapter()
+        setupClicks()
+    }
+
+    private fun setupAdapter() {
         playlistAdapter = PlaylistAdapter()
         binding.playlistRV.adapter = playlistAdapter
         binding.playlistRV.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun setupClicks() {
+        binding.createPlaylistBtn.setOnClickListener {
+            createPlaylistDialog()
+        }
+
+    }
+
+    private fun createPlaylistDialog() {
+        val view = layoutInflater.inflate(R.layout.playlist_add_dialog, null)
+        val createDialog = AlertDialog.Builder(requireContext()).create()
+        createDialog.setView(view)
+
+        val playlistNameEt = view.findViewById<TextInputEditText>(R.id.playlistNameEt)
+        val userNameEt = view.findViewById<TextInputEditText>(R.id.userNameEt)
+
+        val playlistName = playlistNameEt?.text.toString()
+        val userName = userNameEt?.text.toString()
+
+        val addBtn = view.findViewById<AppCompatTextView>(R.id.addBtn)
+
+        addBtn?.setOnClickListener {
+
+            createDialog.dismiss()
+        }
+
+        createDialog.show()
     }
 
     override fun onDestroyView() {
